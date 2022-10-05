0b03dba625a10be95ca559db363e8093b36a6da4
ValerioScharizza <valerio.schiarizza@live.it>
2022-10-06 00:05:52 +0200

    Created footer component

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div id="top-half-footer"></div>
+
+    <div id="bottom-half-footer"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FooterComponent",
+};
+</script>
+
+<style lang="scss" scoped>
+#top-half-footer {
+  min-height: 485px;
+  background-color: #121314;
+}
+
+#bottom-half-footer {
+  min-height: 120px;
+  background-color: #161718;
+}
+</style>
